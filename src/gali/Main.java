49e3e83d34bbd36f0c2a083c024c470c97b623c5
Galiexe <gryphon.c.miller@gmail.com>
2024-11-16 23:24:47 -0600

    Added Basic Personal Info Java Application

---
+++ src/gali/Main.java
@@ -0,0 +1,18 @@
+package gali;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		String firstName = "Phoenix";
+		String lastName = "Miles";
+		String address = "1234 Smith.St";
+		String city = "Denver";
+		String zipCode = "80014";
+	
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(address);
+		System.out.println(city);
+		System.out.println(zipCode);
+	}
+}
